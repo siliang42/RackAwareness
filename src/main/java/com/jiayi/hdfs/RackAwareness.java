699d1c7c2d7cc84d3d9580ba20349c1aef780856
siliang42 <obanie@qq.com>
2019-01-03 00:00:07 +0800

    Initial commit

---
+++ src/main/java/com/jiayi/hdfs/RackAwareness.java
@@ -0,0 +1,52 @@
+package com.jiayi.hdfs;
+
+import org.apache.hadoop.net.DNSToSwitchMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: CONCENTRATE
+ * @time: 2019/1/2.
+ */
+public class RackAwareness implements DNSToSwitchMapping {
+
+    @Override
+    public List<String> resolve(List<String> names) {
+        //ips jiayi1 192.168.206.101
+        List<String> racks = new ArrayList<>();
+        //获取机架ip
+        if(names != null && names.size()> 0){
+            for(String name : names){
+                int hostNum = 0;
+                if(name.startsWith("jiayi")){
+                    hostNum = Integer.parseInt(name.substring("jiayi".length())) + 100;
+                }else if(name.startsWith("192")){
+                    hostNum = Integer.parseInt(name.substring(name.lastIndexOf(".") + 1));
+                }else {
+                    throw new RuntimeException("name value error");
+                }
+
+                //自定义 机架感知 rack1 jiayi1 jiayi2
+                //              rack2 jiayi3 jiayi4
+                if(hostNum < 103){
+                    racks.add("/rack1/" + hostNum);
+                }else {
+                    racks.add("/rack2/" +hostNum);
+                }
+            }
+        }
+
+        return racks;
+    }
+
+    @Override
+    public void reloadCachedMappings() {
+
+    }
+
+    @Override
+    public void reloadCachedMappings(List<String> names) {
+
+    }
+}
